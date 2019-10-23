bb3c91b2822f54512e25a43b8e5e2e49d7de7d96
Hiren Patel <nex.hirenpatel@gmail.com>
2019-10-22 18:06:36 -0700

    exit added

--- src/error.c
+++ src/error.c
@@ -16,12 +16,14 @@ int			usage(int x)
 {
 	(void)x;
 	ft_printf("usage: ft_ssl command [command opts] [command args]\n");
+	exit(1);
 	return (-1);
 }
 
 int			invalid_flag(char invalid_flag)
 {
 	ft_printf("md5: illegal option -- %c\n", invalid_flag);
+	exit(1);
 	return (-1);
 }
 
@@ -37,12 +39,14 @@ int			invalid_command(char *invalid_command)
 	while (++i < g_valid_commands)
 		ft_printf("%s\n", g_command[i]);
 	ft_printf("\nCipher commands:\n");
+	exit(1);
 	return (-1);
 }
 
 int			invalid_string(void)
 {
 	ft_printf("md5: option requires an argument --s\n");
+	exit(1);
 	return (-1);
 }
 
@@ -50,5 +54,6 @@ int			invalid_file(char *invalid_file, char *command_val)
 {
 	ft_printf("ft_ssl: %s: %s: No such file or directory or can't access\n",
 				command_val, invalid_file);
+	exit(1);
 	return (-1);
 }
